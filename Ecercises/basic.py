97f6a968c9fbcb42a0b1ef492e7adbe48224f331
Erick Villarroel <evillarr@espol.edu.ec>
2023-12-01 01:20:23 -0500

    Good

--- Ecercises/basic.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import requests
-
-r = requests.get(url="https://es.wikipedia.org/wiki/Copa_Mundial_de_F%C3%BAtbol_de_2022"
-                 ,params=None, verify=False)
-tables = pd.read_html(r)
-tables
